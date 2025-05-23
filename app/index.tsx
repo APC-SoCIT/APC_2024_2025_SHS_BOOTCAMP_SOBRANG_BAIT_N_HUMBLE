24e37a5ce45f3006622ebcf57b5e5304b40b7904
JoseFrancisR <168408337+JoseFrancisR@users.noreply.github.com>
2025-05-23 14:25:41 +0800

    base_app

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-black"
+    >
+      <Text className="text-white bg-black">Bakit ang sobrang talino ko</Text>
+    </View>
+  );
+}
